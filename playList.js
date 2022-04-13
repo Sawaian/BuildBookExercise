0c11a9feec03a134c04dcf10450319ddc61fbe38
Keith <Keith.macario@gmail.com>
2022-04-12 19:18:10 -0700

    Created playlist object from json.

--- playList.js
+++ playList.js
@@ -1,2 +1,18 @@
-const data = require('./changes.json')
-console.log(data)
+
+
+const playlist = () => {
+    const playData = require('./spotify.json')
+let playlist;
+
+try {
+   playlist = JSON.parse(playData);
+}
+catch (error) {
+    console.log('Error parsing JSON:', error, playData);
+}
+
+
+console.log(playlist)
+}
+
+export default playlist;
